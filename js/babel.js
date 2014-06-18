f78ae7c956abccb965c2310c0de7a0bdb206cdc3
Larry Wu <lw7360@gmail.com>
2014-06-18 13:40:16 -0700

    Added Annotated Source, and cleaned up babel.js once again

--- js/babel.js
+++ js/babel.js
@@ -1,19 +1,50 @@
+// Babel is an open source chat widget and API built with the [PubNub Global 
+// Realtime Network](http://www.pubnub.com). Babel let's you send encrypted, 
+// self-destructing messages and also helps facilitate public key exchange. 
+//
+// Check out the demo right [here](http://larrywu.com/babel/), 
+// and view the source on [Github](https://github.com/lw7360/babel).
+
+// Setup
+// ---
 function Babel(username) {
-    var AppName = "babel"; // The name of our web app, which is also going to be the PubNub channel we subscribe to.
-    var RSAkey = cryptico.generateRSAKey(1024); // Generating a RSA key with the Cryptico JS library.
-    var publicKey = cryptico.publicKeyString(RSAkey); // Grabbing the public portion of the RSAkey we just generated.
+    // The name of the PubNub channel we will be using.
+    var channel = "babel";
+    
+    // Generate an RSA key and grab the public key.
+    var RSAkey = cryptico.generateRSAKey(1024);
+    var publicKey = cryptico.publicKeyString(RSAkey);
+    
+    // A user object that will be given to other users.
     var myUser = {
         username: username,
         publicKey: publicKey
-    }; // Create a user object that contains our username and public key.
-    var users = {}; // An object that contains all the users subscribed to the channel.
-    var messages = {}; // An object that contains received messages.
-    var latestUpdate = 0; // The timestamp of the latest change in presence
+    };
 
+    // A mapping of all usernames to their public keys.
+    var users = {};
+    
+    // A mapping of usernames and the messages they've sent.
+    // (Messages you've sent are mapped by the the username of the reciever)
+    var messages = {};
+    
+    // The timestamp of the latest change in presence.
+    var latestUpdate = 0;
+
+    // 'receiveMessage' and 'presenceChange' are called when a message 
+    // intended for the user is received and when someone connects to 
+    // or leaves the PubNub channel. Both methods can be changed by the
+    // publicly accessible methods 'onMessage' and 'onPresence' respectively.
+    var receiveMessage = function(){};
+    var presenceChange = function(){};
+
+    // messageHandler
+    // ---
+    // 'messageHandler' is called whenever a message is received.
+    // If the recipient of the message is this user, it will decrypt
+    // and store the message inside the messages object,
+    //  then call 'receiveMessage' with the decrypted message.
     var messageHandler = function (msg) {
-        // 'messageHandler' is called whenever a message is received.
-        // If the recipient of the message is this user, it will decrypt
-        // and store the message inside the messages object, then call 'receiveMessage' with the decrypted message.
         if (msg.recipient === username) {
             var plaintext = cryptico.decrypt(msg.message.cipher, RSAkey).plaintext;
             parsedMsg = {
@@ -34,37 +65,44 @@ function Babel(username) {
         }
     };
 
+    // presenceHandler
+    // ---
+    // `presenceHandler` is called whenever a user leaves, joins, or
+    // times out of Babel. After updating our `users` object, it calls
+    // `presenceChange()`.
     var presenceHandler = function (msg) {
         if (msg.timestamp > latestUpdate) {
+            // A user has joined Babel, so we add them to our users object.
             if (msg.action === "join" || msg.action === "state-change") {
-                if ("data" in msg) { // if the presence message contains data aka state, add this to our users object. 
+                // If the presence message contains data aka *state*, add this to our users object. 
+                if ("data" in msg) { 
                     users[msg.data.username] = msg.data.publicKey;
-                } else { // otherwise, we have to call 'here_now' to get the state of the new subscriber to the channel.
+                } 
+
+                // Otherwise, we have to call `here_now` to get the state of the new subscriber to the channel.
+                else { 
                     pubnub.here_now({
-                        channel: AppName,
+                        channel: channel,
                         state: true,
                         callback: herenowUpdate
                     });
                 }
                 presenceChange();
-            } else if (msg.action === "timeout" || msg.action === "leave") {
-                // A user has left or timed out of doge_chat so we remove them from our users object.
+            } 
+            // A user has left or timed out of Babel so we remove them from our users object.
+            else if (msg.action === "timeout" || msg.action === "leave") {
                 delete users[msg.uuid];
                 presenceChange();
-            } else {
-                // Some other kind of presence event has happened. Might as well check the entire channel.
-                pubnub.here_now({
-                    channel: AppName,
-                    state: true,
-                    callback: herenowUpdate,
-                });
-            }
+            } 
         }
     };
 
+    // Babel Private Methods
+    // ---
+    // `herenowUpdate` is only called as a callback to 'here_now'. 
+    // It does a complete update of our users object and then
+    // calls `presenceChange()`.
     var herenowUpdate = function (msg) {
-        // This function is only called as a callback to 'here_now'. 
-        // It does a complete refresh of our users object.
         users = {};
         for (var i = 0; i < msg.uuids.length; i++) {
             if ("state" in msg.uuids[i]) {
@@ -74,39 +112,10 @@ function Babel(username) {
         presenceChange();
     };
 
-    var receiveMessage = function(){};
-    var presenceChange = function(){};
-    // 'receiveMessage' and 'presenceChange' are called when a message intended for the user is received
-    // and when someone connects to or leaves the PubNub channel. They are able to be changed
-    // from outside the object with 'onMessage' and 'onPresence' respectively.
-
-
-    var pubnub = PUBNUB.init({
-        publish_key: 'demo', // Change to your PubNub publish_key
-        subscribe_key: 'demo', // Change to your PubNub subscribe_key
-        uuid: username,
-        ssl: true,
-    });
-
-    pubnub.subscribe({
-        channel: AppName,
-        callback: function (m) {
-            messageHandler(m);
-        },
-        presence: presenceHandler,
-        // connect: function() {
-        //     pubnub.here_now({
-        //         channel: AppName,
-        //         state: true,
-        //         callback: herenowUpdate
-        //     })
-        // },
-        state: myUser, // Set our state to our user object that contains our username and public key.
-        // heartbeat: 10
-    });
-
+    // Delete a message from 'messages' object, after `TTL` seconds.
+    // (If you *sent* the message, username is the username of the recipient.
+    // If you *received* the message, username is the username of the sender.)
     var deleteMessage = function (username, msgID, TTL) {
-        // Delete a message from 'messages' object by the msgID and username of the conversation, after TTL seconds
         setTimeout(function() {
             if (username === msgID) {
                 for (var i = 0; i < messages[username].length; i++) {
@@ -119,20 +128,46 @@ function Babel(username) {
         }, 1000*TTL);
     };
 
+
+
+    // Starting up PubNub
+    // ---
+    // Initialize PubNub.
+    var pubnub = PUBNUB.init({
+        // You can replace `demo` with your own PubNub publish and subscribe keys.
+        publish_key: 'demo',
+        subscribe_key: 'demo',
+
+        uuid: username,
+        ssl: true,
+    });
+
+    // Subscribe to our PubNub channel.
+    pubnub.subscribe({
+        channel: channel,
+        callback: messageHandler,
+        presence: presenceHandler,
+        // Set our state to our user object, which contains our username and public key.
+        state: myUser,
+    });
+
+
+
+    // babel public methods
+    // ---
     return {
+        // Sends `message` to `recipient`. After `ttl` seconds, the message
+        // will self-destruct, and neither you or the recipient will be able 
+        // to retrieve the message from your `messages` object.
         sendMessage: function (recipient, message, ttl) {
-            // recipient = username of the user you are sending your message to.
-            // message = the message you are sending.
-            // ttl = number of seconds the message should be able to be viewed.
             if (recipient in users) {
                 var plaintext = message;
                 var recipient_key = users[recipient];
                 message = cryptico.encrypt(message, recipient_key);
 
                 pubnub.uuid(function (msgID) {
-                    // var msgID = msgID.replace(/-/g,"");
                     pubnub.publish({
-                        channel: AppName,
+                        channel: channel,
                         message: {
                             recipient: recipient,
                             msgID: msgID,
@@ -160,32 +195,34 @@ function Babel(username) {
                 });
             }
         },
+        // callback = function that is called on a received message destined for you
+        // Messages have the form {msgID: "487f703e-3189-4f66-87a1-62cb0ffb52fd", plaintext: "very example message", TTL: 5, sender: "doge", recipient: "shibe"};
         onMessage: function (callback) {
-            // callback = function that is called on a received message destined for you
-            // Messages have the form {msgID: "487f703e-3189-4f66-87a1-62cb0ffb52fd", plaintext: "very example message", TTL: 5, sender: "doge", recipient: "shibe"};
             receiveMessage = callback;
         },
+        // Change the function that is called when a user joins, leaves, or
+        // times out of Babel.
         onPresence: function (callback) {
-            // callback = function that is called when a user has joined/left/timed out of babel.
             presenceChange = callback;
         },
+        // Returns a mapping of all usernames and their respective public keys.
         listUsers: function () {
-            // Returns all users and their associated public keys
             return users;
         },
+        // Returns a mapping of usernames and the messages they've sent.
+        // (Messages you've sent are mapped by the the username of the reciever)
         returnMessages: function () {
-            // Return all messages.
-            console.log(messages);
             return messages;
         },
+        // Returns your RSAkey.
         myKey: function () {
-            // Return your RSAkey.
             return RSAkey;
         },
+        // Quits Babel. Other users will no longer be able to retrieve your 
+        // public key or send messages to you.
         quit: function () {
-            // Quit Babel. Other users will be unable to retrieve your public key or send messages to you.
             pubnub.unsubscribe({
-                channel: AppName
+                channel: channel
             });
         }
     };
