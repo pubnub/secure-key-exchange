e1a8ce4e3004cadaaf1c7a0e8e3583935dfcecad
Larry Wu <lw7360@gmail.com>
2014-07-21 14:32:43 -0700

    Added some helpful tips

--- js/babel-ui.js
+++ js/babel-ui.js
@@ -36,7 +36,10 @@ function BabelUI(username) {
         if (e.keyCode === 13) {
             var myMsg = cleanJS($("#myMessage").text());
             var ttl = $("#TTL").text();
-            if (currentRecipient != null && currentRecipient in babel.listUsers() && myMsg.length > 0 && ttl.length > 0) {
+            if (currentRecipient != null && currentRecipient in babel.listUsers()) {
+                $("#messages").html('&#8592; Select a User to Message first!');
+            }
+            else if (myMsg.length > 0 && ttl.length > 0) {
                 $("#myMessage").text('');
                 babel.sendMessage(currentRecipient, myMsg, parseInt(ttl));
             }
